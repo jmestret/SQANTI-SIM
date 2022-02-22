9ee6de866fc65a98ba7e949ffbb1525b86b0d99e
jorgemt98 <jorgemartineztomas@.com>
2022-02-22 13:09:33 +0100

    feat: new sim modes

--- sqanti_sim.py
+++ sqanti_sim.py
@@ -16,27 +16,17 @@ import argparse
 import os
 import sys
 from collections import defaultdict
-from src import classif_modif_gtf
-from src import expr_file_fixed_count
+from src import classif_gtf
+from src import expr_file
 from src import pb_ont_sim
 from src import sqanti3_stats
 
 def classif():
     parser = argparse.ArgumentParser(prog='sqanti_sim.py classif', description='sqanti_sim.py classif parse options')
     parser.add_argument('--gtf', default = False,  help = '\t\tReference annotation in GTF format', required=True)
-    parser.add_argument('--cat', default = False,  help = '\t\tFile with transcripts structural categories generated with SQANTI-SIM')
     parser.add_argument('-o', '--output', default='sqanti_sim', help = '\t\tPrefix for output files')
     parser.add_argument('-d', '--dir', default='.', help = '\t\tDirectory for output files. Default: Directory where the script was run')
-    parser.add_argument("--min_ref_len", type=int, default=0, help="\t\tMinimum reference transcript length (default: 0 bp as in largasp challenge 1 evaluation)")
-    parser.add_argument('--ISM', default='0', type=int, help = '\t\tNumber of incomplete-splice-matches to delete')
-    parser.add_argument('--NIC', default='0', type=int, help = '\t\tNumber of novel-in-catalog to delete')
-    parser.add_argument('--NNC', default='0', type=int, help = '\t\tNumber of novel-not-in-catalog to delete')
-    parser.add_argument('--Fusion', default='0', type=int, help = '\t\tNumber of Fusion to delete')
-    parser.add_argument('--Antisense', default='0', type=int, help = '\t\tNumber of Antisense to delete')
-    parser.add_argument('--GG', default='0', type=int, help = '\t\tNumber of Genic-genomic to delete')
-    parser.add_argument('--GI', default='0', type=int, help = '\t\tNumber of Genic-intron to delete')
-    parser.add_argument('--Intergenic', default='0', type=int, help = '\t\tNumber of Intergenic to delete')
-    parser.add_argument('--read_only', action='store_true', help = '\t\tIf used the program will only categorize the GTF file but skipping writing a new modified GTF')
+    parser.add_argument('--min_ref_len', type=int, default=0, help='\t\tMinimum reference transcript length (default: 0 bp as in largasp challenge 1 evaluation)')
     parser.add_argument('-k', '--cores', default='1', type=int, help = '\t\tNumber of cores to run in parallel')
 
     args, unknown = parser.parse_known_args()
@@ -44,35 +34,12 @@ def classif():
     if unknown:
         print('classif mode unrecognized arguments: {}\n'.format(' '.join(unknown)), file=sys.stderr)
 
-    if not args.cat:
-        # Classify GTF transcripts in SQANTI3 structural categories
-        trans_info = classif_modif_gtf.classify_gtf(args)
-        cat_out = os.path.join(args.dir, (args.output + '_categories.txt'))
-        args.cat = cat_out
+    # Classify GTF transcripts in SQANTI3 structural categories
+    trans_info = classif_gtf.classify_gtf(args)
 
-    if not args.read_only:
-        # Modify reference GTF
-        counts_end = classif_modif_gtf.simulate_gtf(args)
-
-    if not args.cat:
-        # Print summary table
-        print('***Summary table from categorization\n')
-        classif_modif_gtf.summary_table_cat(trans_info)
-
-    if not args.read_only:
-        print('***Summary table from GTF modification\n')
-        counts_ini = defaultdict(lambda: 0, {
-            'full-splice_match': 0,
-            'incomplete-splice_match': args.ISM,
-            'novel_in_catalog': args.NIC,
-            'novel_not_in_catalog':args.NNC,
-            'fusion' : args.Fusion,
-            'antisense': args.Antisense,
-            'genic_intron': args.GI,
-            'genic' :args.GG,
-            'intergenic':args.Intergenic
-        }) 
-        classif_modif_gtf.summary_table_del(counts_ini, counts_end)
+    # Print summary table
+    print('***Summary table from categorization\n')
+    classif_gtf.summary_table_cat(trans_info)
 
     return
 
@@ -94,7 +61,7 @@ def expr():
 
     f_out = os.path.join(args.dir, (args.output + '_expression.tsv'))
 
-    expr_file_fixed_count.create_expr_file(args.cat, args.deleted, args.n_trans,
+    classif.create_expr_file(args.cat, args.deleted, args.n_trans,
                                            args.coverage, f_out
     )
 
@@ -105,8 +72,10 @@ def sim():
     parser = argparse.ArgumentParser(prog='sqanti_sim.py sim', description='sqanti_sim.py sim parse options')
     parser.add_argument('--genome', default = False,  help = '\t\tReference genome FASTA')
     parser.add_argument('--gtf', default = False,  help = '\t\tReference annotation in GTF format', required=True)
-    parser.add_argument('--expr', default = False,  help = '\t\tExpression file as formated in README', required=True)
-    parser.add_argument('--read_count', default = 50000, type=int,  help = '\t\tNumber of reads to simulate', required=True)
+    parser.add_argument('--cat', default = False,  help = '\t\tFile with transcripts structural categories generated with SQANTI-SIM')
+    parser.add_argument('-i', '--read', default = False,  help = '\t\tInput reads for quantification')
+    parser.add_argument('-nt', '--n_trans', default = 20000, type=int,  help = '\t\tNumber of different transcripts to simulate', required=True)
+    parser.add_argument('--read_count', default = 100000, type=int,  help = '\t\tNumber of reads to simulate', required=True)
     parser.add_argument('--read_type', default = 'dRNA', type=str,  help = '\t\tRead type for NanoSim simulation', required=True)
     parser.add_argument('-o', '--output', default='sqanti_sim', help = '\t\tPrefix for output files')
     parser.add_argument('-d', '--dir', default='.', help = '\t\tDirectory for output files. Default: Directory where the script was run')
@@ -114,13 +83,72 @@ def sim():
     group = parser.add_argument_group('Sequencing technology', 'Choose PacBio or Nanopore reads')
     group.add_argument('--pb', action='store_true', help = '\t\tIf used the program will simulate PacBio reads with IsoSeqSim')
     group.add_argument('--ont', action='store_true', help = '\t\tIf used the program will simulate ONT reads with NanoSim')
-    
+    parser.add_argument('--ISM', default='2000', type=int, help = '\t\tNumber of incomplete-splice-matches to delete')
+    parser.add_argument('--NIC', default='2000', type=int, help = '\t\tNumber of novel-in-catalog to delete')
+    parser.add_argument('--NNC', default='2000', type=int, help = '\t\tNumber of novel-not-in-catalog to delete')
+    parser.add_argument('--Fusion', default='0', type=int, help = '\t\tNumber of Fusion to delete')
+    parser.add_argument('--Antisense', default='0', type=int, help = '\t\tNumber of Antisense to delete')
+    parser.add_argument('--GG', default='0', type=int, help = '\t\tNumber of Genic-genomic to delete')
+    parser.add_argument('--GI', default='0', type=int, help = '\t\tNumber of Genic-intron to delete')
+    parser.add_argument('--Intergenic', default='0', type=int, help = '\t\tNumber of Intergenic to delete')
+    parser.add_argument('-m', '--mode', default = '',  help = '\t\tQuantification mode: equal (all transcripts have same coverage), low (novel will have less coverage than known) or sample (given a fastq file with reads get count distribution)')
+    parser.add_argument('--nbn_known',type=str,default='50',help="Average read count per transcript to simulate (i.e., the parameter 'n' of the Negative Binomial distribution)")
+    parser.add_argument('--nbp_known',type=str,default='0.5',help="The parameter 'p' of the Negative Binomial distribution")
+    parser.add_argument('--nbn_novel',type=str,default='5',help="Average read count per transcript to simulate (i.e., the parameter 'n' of the Negative Binomial distribution)")
+    parser.add_argument('--nbp_novel',type=str,default='0.5',help="The parameter 'p' of the Negative Binomial distribution")
+    parser.add_argument("--reference_transcripts", "-r", help="reference transcripts in FASTA format", type=str)
+
     args, unknown = parser.parse_known_args()
 
     if unknown:
         print('sim mode unrecognized arguments: {}\n'.format(' '.join(unknown)), file=sys.stderr)
 
-    args.output = cat_out = os.path.join(args.dir, args.output)
+    # Modify GTF
+    counts_end = pb_ont_sim.simulate_gtf(args)
+    print('***Summary table from GTF modification\n')
+    counts_ini = defaultdict(lambda: 0, {
+        'full-splice_match': 0,
+        'incomplete-splice_match': args.ISM,
+        'novel_in_catalog': args.NIC,
+        'novel_not_in_catalog':args.NNC,
+        'fusion' : args.Fusion,
+        'antisense': args.Antisense,
+        'genic_intron': args.GI,
+        'genic' :args.GG,
+        'intergenic':args.Intergenic
+    }) 
+    pb_ont_sim.summary_table_del(counts_ini, counts_end)
+
+    expression_out = os.path.join(args.dir, (args.output + '_expression.tsv'))
+    deleted_out = os.path.join(args.dir, (args.output + '_deleted.txt'))
+
+    if mode == 'equal':
+        pb_ont_sim.create_expr_file_fixed_count(args.cat, deleted_out, args.n_trans,
+                                            args.read_count, expression_out
+        )
+    elif mode == 'custom':
+        pb_ont_sim.create_expr_file_nbinom(args.cat, deleted_out, args.n_trans,
+                                            args.nbn_known, args.nbp_known, 
+                                            args.nbn_novel, args.nbp_novel,
+                                            expression_out
+        )
+    
+    elif mode == 'sample':
+        if args.pb:
+            pb_ont_sim.create_expr_file_sample(args.cat, deleted_out, 
+                                            args.reference_transcripts, args.reads,
+                                            expression_out, 'pb'
+            )
+        if args.ont:
+            pb_ont_sim.create_expr_file_sample(args.cat, deleted_out, 
+                                            args.reference_transcripts, args.reads,
+                                            expression_out, 'ont'
+            )
+    
+    else:
+        print('Not valid sim mode', file=sys.stderr)
+
+    args.output = os.path.join(args.dir, args.output)
 
     if args.pb:
         pb_ont_sim.pb_simulation(args)
--- src/pb_ont_sim.py
+++ src/pb_ont_sim.py
@@ -7,11 +7,385 @@ Generate counts for sim
 @date 20/02/2022
 '''
 
+from importlib.metadata import distribution
 import logging
 import os
+import sys
 import subprocess
+import random
+import numpy
+import pysam
 from collections import defaultdict
 
+
+#------------------------------------
+# MODIFY GTF FUNCTIONS
+
+def target_trans(f_name: str, f_name_out: str, counts: dict)-> tuple:
+    '''
+    Choose those transcripts that will be deleted from the original GTF
+    to generate the modified file to use as the reference annotation
+
+    Args:
+        f_name (str) name of the file with the GTF classification
+        counts (dict) dictinary with the number of transcripts of each SC to be
+                      deleted
+    '''
+
+    trans_by_SC = defaultdict(lambda: [])
+    trans_by_gene = defaultdict(lambda: [])
+
+    target_trans = set()
+    target_genes = set()
+    ref_trans = set()
+    ref_genes = set()
+
+    # Build a list for each SC with all transcripts that were classified there
+    with open(f_name, 'r') as gtf:
+        header = gtf.readline()
+        for line in gtf:
+            line_split = line.split()
+            gene = line_split[1]
+            SC = line_split[2]
+
+            trans_by_SC[SC].append(tuple(line_split))
+            trans_by_gene[gene].append(tuple(line_split))
+    
+    gtf.close()
+
+    f_out = open(f_name_out, 'w')
+    f_out.write('TransID\tGeneID\tSC\tRefGene\tRefTrans\tTSS\tTTS\tDonors\tAcceptors\n')
+
+    # Select randomly the transcripts of each SC that are going to be deleted
+    # It's important to make sure you don't delete its reference trans or gene
+    for SC in counts:
+        if counts[SC] > 0:
+            SCtrans = trans_by_SC[SC]
+            random.Random(1234).shuffle(SCtrans)
+            for trans in SCtrans:
+                trans_id = trans[0]
+                gene_id = trans[1]
+                SC = trans[2]
+                ref_g = trans[3]
+                ref_t = trans[4]
+
+                if SC in ['full-splice_match', 'incomplete-splice_match', 'genic_intron'] and counts[SC] > 0:
+                    if trans_id not in ref_trans and gene_id not in ref_genes and gene_id not in target_genes and ref_t not in target_trans:
+                        target_trans.add(trans_id)
+                        target_genes.add(gene_id)
+                        ref_trans.add(ref_t)
+                        counts[SC] -= 1
+                        f_out.write('\t'.join(trans))
+                        f_out.write('\n')
+
+                elif SC in ['novel_in_catalog', 'novel_not_in_catalog'] and counts[SC] > 0:
+                    if trans_id not in ref_trans and gene_id not in ref_genes and gene_id not in target_genes and ref_g not in target_genes:
+                        target_trans.add(trans_id)
+                        target_genes.add(gene_id)
+                        ref_genes.add(ref_g)
+                        counts[SC] -= 1
+                        f_out.write('\t'.join(trans))
+                        f_out.write('\n')
+
+                elif SC in ['fusion', 'antisense', 'genic'] and counts[SC] > 0:
+                    if trans_id not in ref_trans and gene_id not in ref_genes and gene_id not in target_genes:
+                        ref_g = trans[3].split('_')
+                        for i in ref_g:
+                            if i in target_genes:
+                                break
+                        else:
+                            target_trans.add(trans_id)
+                            target_genes.add(gene_id)
+                            for i in ref_g:
+                                ref_genes.add(i)
+                            counts[SC] -= 1
+                            f_out.write('\t'.join(trans))
+                            f_out.write('\n')
+                
+                elif SC == 'intergenic' and counts[SC] > 0:
+                    if trans_id not in ref_trans and gene_id not in ref_genes and gene_id not in target_genes:
+                        target_trans.add(trans_id)
+                        target_genes.add(gene_id)
+                        counts[SC] -= 1
+                        f_out.write('\t'.join(trans))
+                        f_out.write('\n')
+                
+                if counts[SC] <= 0:
+                    break
+
+    final_target = target_trans
+    for gene in trans_by_gene:
+        for trans in trans_by_gene[gene]:
+            if trans[0] in target_trans:
+                trans_by_gene[gene].remove(trans)
+                if len(trans_by_gene[gene]) == 0:
+                    final_target.add(gene)
+
+    f_out.close()
+    return final_target
+    #return target_trans, ref_genes, ref_trans
+
+
+def getGeneID(line: str)-> str:
+    '''
+    Returns the gene_id of a GTF line
+
+    Args:
+        line (str) line readed from GTF file
+
+    Returns:
+        gene_id (str) gene_id from that feature
+    '''
+
+    line_split = line.split()
+    gene_id = line_split[line_split.index('gene_id') + 1]
+    gene_id = gene_id.replace(';', '').replace('"', '')
+    
+    return gene_id
+
+
+def getTransID(line: str)-> str:
+    '''
+    Returns the transcript_id of a GTF line
+
+    Args:
+        line (str) line readed from GTF file
+
+    Returns:
+        trans_id (str) transcript_id from that feature
+    '''
+
+    try:
+        line_split = line.split()
+        trans_id = line_split[line_split.index('transcript_id') + 1]
+        trans_id = trans_id.replace(';', "").replace('"', "")
+    except:
+        trans_id = None
+
+    return trans_id
+
+
+def modifyGTF(f_name_in: str, f_name_out: str, target: list):
+    '''
+    Modify the original GTF deleting target transcripts to simulate specific
+    SQANTI3 structural categorires
+
+    Args:
+        f_name_in (str) file name of the reference annotation GTF
+        f_name_out (str) file name of the modified GTF generated
+        target_trans (list) list of transcripts that will be deleted
+        ref_genes (list) list of genes that can't be deleted
+    '''
+    
+    f_out = open(f_name_out, 'w')
+
+    with open(f_name_in, 'r') as gtf_in:
+        for line in gtf_in:
+            if line.startswith('#'):
+                f_out.write(line)
+            else:
+                gene_id = getGeneID(line)
+                trans_id = getTransID(line)
+                if gene_id in target or trans_id in target:
+                    pass
+                else:
+                    f_out.write(line)
+    gtf_in.close()
+    f_out.close()
+
+    return
+
+
+def simulate_gtf(args, cat_in):
+    print('***Writting modified GTF\n')
+    counts = defaultdict(lambda: 0, {
+        'full-splice_match': 0,
+        'incomplete-splice_match': args.ISM,
+        'novel_in_catalog': args.NIC,
+        'novel_not_in_catalog':args.NNC,
+        'fusion' : args.Fusion,
+        'antisense': args.Antisense,
+        'genic_intron': args.GI,
+        'genic' :args.GG,
+        'intergenic':args.Intergenic
+    })
+
+    gtf_modif = os.path.join(args.dir, (args.output + '_modified.gtf'))
+    del_trans = os.path.join(args.dir, (args.output + '_deleted.txt'))
+
+    target = target_trans(args.cat, del_trans, counts)
+    modifyGTF(args.gtf, gtf_modif, target)
+
+    return counts
+
+
+def summary_table_del(counts_ini: dict, counts_end: dict):
+    for sc in counts_end:
+        counts_ini[sc] = counts_ini[sc] - counts_end[sc]
+    
+    print('\033[94m_' * 79 + '\033[0m')
+    print('\033[92mS Q A N T I - S I M\033[0m \U0001F4CA')
+    print()
+    print('GTF modification summary Table \U0001F50E')
+    print('\033[94m_' * 79 + '\033[0m')
+    for k, v in counts_end.items():
+        print('\033[92m|\033[0m ' + k + ': ' + str(v))
+
+
+def create_expr_file_fixed_count(f_cat: str, f_del: str, n_trans: int, read_count: int, output: str):
+
+    deleted = set()
+    with open(f_del, 'r') as del_in:
+        skip = del_in.readline()
+        for line in del_in:
+            line = line.split()
+            deleted.add(line[0])
+    del_in.close()
+
+
+    tot_trans = deleted
+    with open(f_cat, 'r') as cat_in:
+        skip = cat_in.readline()
+        for line in cat_in:
+            trans = line.split()[0]
+            if trans not in deleted:
+                tot_trans.add(trans)
+            if len(tot_trans) == n_trans:
+                break
+
+    cat_in.close()
+    
+
+    coverage = read_count//n_trans
+    tpm = (1000000.0 * coverage) / (coverage * n_trans) # Not taking into account transcript length
+    f_out = open(output, 'w')
+    f_out.write('target_id\test_counts\ttpm\n')
+    for trans in tot_trans:
+        f_out.write(trans + '\t' + str(coverage) + '\t' + tpm + '\n')
+    f_out.close()
+
+def create_expr_file_nbinom(f_cat: str, f_del: str, n_trans, nbn_known, nbp_known, nbn_novel, nbp_novel, output: str):
+    deleted = set()
+    with open(f_del, 'r') as del_in:
+        skip = del_in.readline()
+        for line in del_in:
+            line = line.split()
+            deleted.add(line[0])
+    del_in.close()
+
+
+    tot_trans = deleted
+    with open(f_cat, 'r') as cat_in:
+        skip = cat_in.readline()
+        for line in cat_in:
+            trans = line.split()[0]
+            if trans not in deleted:
+                tot_trans.add(trans)
+            if len(tot_trans) == n_trans:
+                break
+
+    cat_in.close()
+
+    nb_known = numpy.random.negative_binomial(nbn_known,nbp_known,len(tot_trans)-len(deleted)).tolist()
+    nb_novel = numpy.random.negative_binomial(nbn_novel,nbp_novel,len(deleted)).tolist()
+
+    f_out = open(output, 'w')
+    f_out.write('target_id\test_counts\ttpm\n')
+    i_known = 0
+    i_novel = 0
+    for trans in tot_trans:
+        if trans in deleted:
+            coverage = nb_novel[i_novel]
+            i_novel += 1
+        else:
+            coverage = nb_known[i_known]
+            i_known += 1
+        tpm = (1000000.0 * coverage) / (coverage * n_trans)  
+        f_out.write(trans + '\t' + str(coverage) + '\t' + tpm + '\n')
+    f_out.close()
+
+
+def create_expr_file_sample(f_cat: str, f_del: str, ref_trans,reads, output: str, tech):
+    sam_file = ''.join(output.split[:-1]) + '_' + tech + '.sam'
+
+    if tech == 'pb':
+        res = subprocess.run(['minimap2', ref_trans, reads, '-x', 'map-pb',
+                              '-a', '--secondary=no', '-o', sam_file
+        ])
+    elif tech == 'ont':
+        res = subprocess.run(['minimap2', ref_trans, reads, '-x', 'map-ont',
+                              '-a', '--secondary=no', '-o', sam_file
+        ])
+    
+    if res.returncode != 0:
+        logging.error('minimap2 failed with code %d' %(res.returncode))
+        sys.exit(1)
+
+    trans_counts = defaultdict(lambda: 0)
+
+    with pysam.AligmentFile(sam_file, 'r') as sam_file_in:
+        for align in sam_file_in:
+            trans_id = align.reference_name
+
+            if align.reference_id == -1 or align.is_supplementary or align.is_secondary:
+                continue
+            trans_counts[trans_id] += 1
+    os.remove(sam_file)
+
+    expr_distr = list(trans_counts.values())
+    expr_distr.sort()
+    n_trans = len(expr_distr)
+
+    deleted = set()
+    with open(f_del, 'r') as del_in:
+        skip = del_in.readline()
+        for line in del_in:
+            line = line.split()
+            deleted.add(line[0])
+    del_in.close()
+
+
+    known_trans = set()
+    with open(f_cat, 'r') as cat_in:
+        skip = cat_in.readline()
+        for line in cat_in:
+            trans = line.split()[0]
+            if trans not in deleted:
+                known_trans.add(trans)
+            if (len(known_trans) + len(deleted)) == n_trans:
+                break
+    cat_in.close()
+
+    lim_novel = (len(deleted)//3)*2
+    lim_known = (len(known_trans)//3)*2
+
+    novel_expr = expr_distr[:lim_novel]
+    known_expr =  expr_distr[lim_known:]
+
+    expr_distr = expr_distr[lim_novel:lim_known]
+    random.shuffle(expr_distr)
+
+    novel_expr.extend(expr_distr[:len(deleted)-lim_novel])
+    known_expr.extend(expr_distr[len(known_trans)-lim_known:])
+
+    f_out = open(output, 'w')
+    f_out.write('target_id\test_counts\ttpm\n')
+
+    for i, trans in enumerate(deleted):
+        coverage = novel_expr[i] 
+        tpm = (1000000.0 * coverage) / (coverage * n_trans)
+        f_out.write(trans + '\t' + str(coverage) + '\t' + tpm + '\n')
+    
+    for i, trans in enumerate(known_trans):
+        coverage = known_expr[i]
+        tpm = (1000000.0 * coverage) / (coverage * n_trans)
+        f_out.write(trans + '\t' + str(coverage) + '\t' + tpm + '\n')
+
+    f_out.close()
+
+    
+
+    
 
 def pb_simulation(args):
     logging.info('***Simulating PacBio reads with IsoSeqSim')
